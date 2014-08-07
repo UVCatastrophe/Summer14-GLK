d390ee79e755138037331275f53454514d285d66
UVCatastrophe <ramici@uchicago.edu>
2014-08-07 15:27:59 -0500

    rewrote camera and added camera features

--- teemstuff/polydemo.cpp
+++ teemstuff/polydemo.cpp
@@ -52,14 +52,17 @@ TwBar *bar;
 struct render_info{
   GLuint vao = -1;
   GLuint buffs[3];
-  GLuint uniforms[3];
+  GLuint uniforms[4];
   GLuint elms;
   ShaderProgram* shader = NULL;
 } render;
 
 struct ui_pos{
   bool isDown = false;
-  int mode; //0 for rotate, 1 for zoom
+  GLuint mouseButton;
+  //0 for all, 1 for fov, 2 for just X, 3 for just y, 4 for just z
+  int mode;
+
   double last_x;
   double last_y;
 } ui;
@@ -68,10 +71,17 @@ struct camera{
   glm::vec3 center = glm::vec3(0.0f,0.0f,0.0f);
   glm::vec3 pos = glm::vec3(3.0f,0.0f,0.0f);
   glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+  float fov = 1.0f;
+  float near_plane= .1f;
+  float far_plane= 100.0f;
+
+  bool fixUp = false;
 } cam;
 
 glm::mat4 view = glm::lookAt(cam.pos,cam.center,cam.up);
-glm::mat4 proj = glm::perspective(1.0f, ((float) width)/((float)height),.1f, 100.0f);
+glm::mat4 proj = glm::perspective(cam.fov, ((float) width)/((float)height),
+				  cam.near_plane, cam.far_plane);
 glm::mat4 model = glm::mat4();
 
 /* -------- Prototypes -------------------------*/
@@ -82,7 +92,37 @@ limnPolyData *generate_spiral(float A, float B,unsigned int thetaRes,
 
 /*---------------------Function Defentions----------------*/
 
-void TWCB_Set(const void *value, void *clientData){
+void update_view(){
+  view = glm::lookAt(cam.pos,cam.center,cam.up);
+} 
+
+void update_proj(){
+  proj = glm::perspective(cam.fov, ((float) width)/((float)height),
+			  cam.near_plane, cam.far_plane);
+}
+
+void TWCB_Cam_Set(const void *value, void *clientData){
+  float* val_vec = (float*)value;
+  float* cd_vec = (float*)clientData;
+
+  cd_vec[0] = val_vec[0];
+  cd_vec[1] = val_vec[1];
+  cd_vec[2] = val_vec[2];
+
+  update_view(); 
+}
+
+void TWCB_Cam_Get(void *value, void *clientData){
+  float* val_vec = (float*)value;
+  float* cd_vec = (float*)clientData;
+
+  val_vec[0] = cd_vec[0];
+  val_vec[1] = cd_vec[1];
+  val_vec[2] = cd_vec[2];
+
+}
+
+void TWCB_Spiral_Set(const void *value, void *clientData){
 #if USE_TIME
   double genStart;
   double buffStart;
@@ -95,8 +135,10 @@ void TWCB_Set(const void *value, void *clientData){
 #if USE_TIME
     genStart = airTime();
 #endif
+
   limnPolyData *lpd = generate_spiral(lpd_alpha,lpd_beta,
 				      lpd_theta,lpd_phi);
+
 #if USE_TIME
      genTime = airTime();
      buffStart = airTime();
@@ -126,7 +168,7 @@ void TWCB_Set(const void *value, void *clientData){
   poly = lpd;
 }
 
-void TWCB_Get(void *value, void *clientData){
+void TWCB_Spiral_Get(void *value, void *clientData){
   *(float *)value = *(float*)clientData;
 }
 
@@ -135,6 +177,7 @@ void mouseButtonCB(GLFWwindow* w, int button,
 
 
   glfwGetCursorPos (w, &(ui.last_x), &(ui.last_y));
+  ui.mouseButton = button;
 
   //User is not currently rotating or zooming.
   if(ui.isDown == false){
@@ -162,8 +205,43 @@ void mouseButtonCB(GLFWwindow* w, int button,
 
   if(ui.last_x <= width*.1)
     ui.mode = 1;
+  else if(ui.last_x >= width*.9)
+    ui.mode = 3;
+  else if(ui.last_y <= height*.1)
+    ui.mode = 4;
+  else if(ui.last_y >= height*.9)
+    ui.mode = 2;
   else
     ui.mode = 0;
+
+}
+
+void rotate_diff(glm::vec3 diff){
+
+  glm::mat4 inv = glm::inverse(view);
+  glm::vec4 invV = inv * glm::vec4(diff,0.0);
+
+  glm::vec3 norm = glm::cross(cam.center-cam.pos,glm::vec3(invV));
+  float angle = (glm::length(diff) * 2*3.1415 ) / width;
+  
+  //Create a rotation matrix around norm.
+  glm::mat4 rot = glm::rotate(glm::mat4(),angle,norm);
+  cam.pos = glm::vec3(rot * glm::vec4(cam.pos,0.0));
+  if(!cam.fixUp)
+    cam.up = glm::vec3(rot*glm::vec4(cam.up,0.0));
+
+  update_view();
+}
+
+void translate_diff(glm::vec3 diff){
+  glm::mat4 inv = glm::inverse(view);
+  glm::vec4 invV = inv * glm::vec4(diff,0.0);
+  
+  glm::mat4 trans = glm::translate(glm::mat4(),
+				   glm::vec3(invV)/(float)width);
+  cam.center = glm::vec3(trans*glm::vec4(cam.center,1.0));
+  cam.pos = glm::vec3(trans*glm::vec4(cam.pos,1.0));
+  update_view();
 }
 
 void mousePosCB(GLFWwindow* w, double x, double y){
@@ -173,31 +251,74 @@ void mousePosCB(GLFWwindow* w, double x, double y){
     return;
   }
 
-  //Rotating Mode
+  float x_diff = ui.last_x - x;
+  float y_diff = ui.last_y - y;
+  
+  //Standard (middle of the screen mode)
   if(ui.mode == 0){
-    float x_diff = ui.last_x - x;
-    float y_diff = ui.last_y - y;
-    glm::vec3 norm = glm::cross(cam.pos-cam.center, glm::vec3(0,y_diff,x_diff));
-    float angle = (glm::length(glm::vec2(x_diff,y_diff)) * 2*3.1415 ) / width;
-    
-    //Update to model matrix to rotate to new orientation.
-    model = glm::rotate(glm::mat4(),angle,norm)* model;
-    
-    ui.last_x = x;
-    ui.last_y = y;
+
+    //Rotate
+    if(ui.mouseButton == GLFW_MOUSE_BUTTON_1){
+      rotate_diff(glm::vec3(-x_diff,y_diff,0.0f));
+    }
+
+    //Translate
+    else if(ui.mouseButton == GLFW_MOUSE_BUTTON_2){
+      translate_diff(glm::vec3(-x_diff,y_diff,0.0f));
+    }
+
   }
   
   //Zooming Mode
   else if(ui.mode == 1){
-    float y_diff = ui.last_y - y;
-    cam.pos += glm::normalize(cam.pos-cam.center) * y_diff*.001f;
 
-    //Change the view matrix to be closer/farther away
-    view = glm::lookAt(cam.pos,cam.center,cam.up);
+    //FOV zoom
+    if(ui.mouseButton == GLFW_MOUSE_BUTTON_1){
+      cam.fov += (-y_diff / height);
+      update_proj();
+    }
+    else if(ui.mouseButton == GLFW_MOUSE_BUTTON_2)
+      std::cout << "here\n";
+      translate_diff(glm::vec3(0.0f,0.0f,y_diff));
 
-    ui.last_x = x;
-    ui.last_y = y;
   }
+
+  //modify u only
+   else if(ui.mode == 4){
+     if(ui.mouseButton == GLFW_MOUSE_BUTTON_1){
+       if(x_diff != 0.0) //Can't rotate by 0
+	 rotate_diff(glm::vec3(-x_diff,0.0f,0.0f));
+     }
+     else if(ui.mouseButton == GLFW_MOUSE_BUTTON_2)
+       translate_diff(glm::vec3(-x_diff,0.0f,0.0f));
+   }
+
+  //modify v only
+   else if(ui.mode == 3){
+     if(ui.mouseButton == GLFW_MOUSE_BUTTON_1){
+       if(y_diff != 0.0)
+	 rotate_diff(glm::vec3(0.0f,y_diff,0.0f));
+     }
+     else if(ui.mouseButton == GLFW_MOUSE_BUTTON_2)
+       translate_diff(glm::vec3(0.0f,y_diff,0.0f));
+   }
+
+  //modify w only
+   else if(ui.mode == 2){
+     if(ui.mouseButton == GLFW_MOUSE_BUTTON_1){
+       float angle = (x_diff*3.1415*2) / width;
+       glm::mat4 rot = glm::rotate(glm::mat4(),angle,
+				   cam.pos-cam.center);
+       cam.pos = glm::vec3(rot * glm::vec4(cam.pos,0.0));
+       if(!cam.fixUp)
+	 cam.up = glm::vec3(rot*glm::vec4(cam.up,0.0));
+       update_view();
+     }
+
+   }
+
+  ui.last_x = x;
+  ui.last_y = y;
 
 }
 
@@ -208,7 +329,7 @@ void screenSizeCB(GLFWwindow* win, int w, int h){
   glViewport(0,0,width,height);
 
   //Update the projection matrix to reflect the new aspect ratio
-  proj = glm::perspective(1.0f, ((float) width)/((float)height),.1f, 100.0f);
+  update_proj();
 
 }
 
@@ -423,17 +544,29 @@ void init_ATB(){
     std::to_string((int)(width - ATB_WIDTH)) + std::string(" 0'");
   TwDefine(s.c_str());
 
-  TwAddVarCB(bar, "ALPHA", TW_TYPE_FLOAT, TWCB_Set, TWCB_Get, 
+  TwAddVarCB(bar, "ALPHA", TW_TYPE_FLOAT, TWCB_Spiral_Set, TWCB_Spiral_Get, 
 	     &lpd_alpha, "min=0.0 step=.01 label=Alpha");
 
-  TwAddVarCB(bar, "BETA", TW_TYPE_FLOAT, TWCB_Set, TWCB_Get, 
+  TwAddVarCB(bar, "BETA", TW_TYPE_FLOAT, TWCB_Spiral_Set, TWCB_Spiral_Get, 
 	     &lpd_beta, "min=0.0 step=.01 label=Beta");
 
-  TwAddVarCB(bar, "THETA", TW_TYPE_FLOAT, TWCB_Set, TWCB_Get, 
+  TwAddVarCB(bar, "THETA", TW_TYPE_FLOAT, TWCB_Spiral_Set, TWCB_Spiral_Get, 
 	     &lpd_theta, "min=1.0 step=1 label=Theta");
 
-  TwAddVarCB(bar, "PHI", TW_TYPE_FLOAT, TWCB_Set, TWCB_Get, 
+  TwAddVarCB(bar, "PHI", TW_TYPE_FLOAT, TWCB_Spiral_Set, TWCB_Spiral_Get, 
 	     &lpd_phi, "min=1.0 step=1 label=Phi");
+
+  TwAddVarRW(bar, "FixUP", TW_TYPE_BOOLCPP, &(cam.fixUp), 
+	     " label='Fix Up Vector' group='Camera'");
+
+  TwAddVarCB(bar, "CamCenter", TW_TYPE_DIR3F,TWCB_Cam_Set, TWCB_Cam_Get, 
+	     glm::value_ptr(cam.center), "label='Center' group='Camera'");
+
+  TwAddVarCB(bar, "CamPos", TW_TYPE_DIR3F, TWCB_Cam_Set, TWCB_Cam_Get, 
+	     glm::value_ptr(cam.pos), "label='Position' group='Camera'");
+
+  TwAddVarCB(bar, "CamUp", TW_TYPE_DIR3F, TWCB_Cam_Set, TWCB_Cam_Get, 
+	     glm::value_ptr(cam.up),"label='Up Vector' group='Camera'");
 
 }
 
